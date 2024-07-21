39c3bfe0cf21ef4a7fe722613574b99b1ce7f087
samwanyua <samexample8@gmail.com>
2024-07-22 00:50:46 +0300

    Get character input from the user

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a new integer");
+        System.out.print("Enter a new integer:");
         int x = scanner.nextInt();
+        System.out.print("Enter a new character:");
+        char c = scanner.next().charAt(0);
         System.out.println( "The new Integer input is " + x);
+        System.out.println( "The new character input is " + c);
     }
 }
