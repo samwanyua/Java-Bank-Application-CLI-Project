1f70893e0cd0c3421f4d5efc74f5049aa0df2a96
samwanyua <samexample8@gmail.com>
2024-07-22 00:51:59 +0300

    Get character input from the user

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a new integer:");
         int x = scanner.nextInt();
+
         System.out.print("Enter a new character:");
         char c = scanner.next().charAt(0);
-        System.out.println( "The new Integer input is " + x);
+
+
+        System.out.println( "The new integer input is " + x);
         System.out.println( "The new character input is " + c);
     }
 }
