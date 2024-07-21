465075bd8884dbf025e9817668c5023281ce3756
samwanyua <samexample8@gmail.com>
2024-07-22 00:44:49 +0300

    Get input from the user

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a new integer");
+        int x = scanner.nextInt();
+        System.out.println( "The new Integer input is " + x);
     }
 }
